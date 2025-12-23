f027cba30bbf1c8935a1ed99ceb7cec8e5653bcb
kunal630 <pundekuna2008@gmail.com>
2025-12-23 21:29:28 +0530

    DrivingLicence cheker

---
+++ DrivingLicence.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class DrivingLicence {
+    public static void main(String[] args) {
+      
+        
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter Your Age : ");
+      int Age=sc.nextInt();
+
+      if(Age<18){
+        System.out.println("You are not eligible to get driving licence");
+      }else{
+        System.out.println("You are not eligible to get driving licence");
+      }
+    }
+}
