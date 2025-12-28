13ede1ef785fe446f616d5c5e16462a7dba0ec19
kunal630 <pundekuna2008@gmail.com>
2025-12-28 17:57:22 +0530

    Third file ADDED

---
+++ Pattern3.java
@@ -0,0 +1,15 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        int num=1;
+        int n=3;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(num);
+                num++;
+        
+            }
+            System.out.println();
+        }
+
+    }
+}
