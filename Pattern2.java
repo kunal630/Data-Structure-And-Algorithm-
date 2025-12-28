6ed5156223ff850cb87ea0217f09c5c52bbe53d1
kunal630 <pundekuna2008@gmail.com>
2025-12-28 17:53:33 +0530

    Second pattern File Added

---
+++ Pattern2.java
@@ -0,0 +1,13 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<n;i++){
+            char ch='A';
+            for(int j=0;j<n;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
