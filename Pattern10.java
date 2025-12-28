8d87108776d0b97a210b770bf69f6255fe252deb
kunal630 <pundekuna2008@gmail.com>
2025-12-28 22:29:40 +0530

    Add

---
+++ Pattern10.java
@@ -0,0 +1,13 @@
+public class Pattern10 {
+    public static void main(String[] args) {
+        int n=4;
+        int num=1;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=i+1;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
