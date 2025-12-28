21f48da53ea9dd03989940fb1406181c58f7b053
kunal630 <pundekuna2008@gmail.com>
2025-12-28 22:26:09 +0530

    Add

---
+++ Pattern9.java
@@ -0,0 +1,11 @@
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j>0;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
