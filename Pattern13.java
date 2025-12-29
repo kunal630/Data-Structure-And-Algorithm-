8b5a2fac87c9a6afd0beb3e019f8b1e805063f38
kunal630 <pundekuna2008@gmail.com>
2025-12-29 15:55:00 +0530

    Space Pattern Added

---
+++ Pattern13.java
@@ -0,0 +1,14 @@
+public class Pattern13 {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<n-i;k++){
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }
+    }
+}
