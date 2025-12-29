75ddcca54b9b21e779b79f296fd62e8a26ed98ca
kunal630 <pundekuna2008@gmail.com>
2025-12-29 15:58:33 +0530

    Space Pattern Added

---
+++ Pattern14.java
@@ -0,0 +1,16 @@
+public class Pattern14 {
+    public static void main(String[] args) {
+        int n=4;
+        char ch='A';
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+             System.out.print(" ");
+            }
+            for(int k=0;k<n-i;k++){
+                    System.out.print(ch);
+             }
+             ch++;
+             System.out.println();
+        }
+    }
+}
