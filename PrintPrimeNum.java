2ebc3a1ccd2c01c2c5dd6d59616c7df8745393f7
kunal630 <pundekuna2008@gmail.com>
2025-12-29 21:47:54 +0530

    Function Prime Number printer  added

---
+++ PrintPrimeNum.java
@@ -0,0 +1,22 @@
+public class PrintPrimeNum {
+    public static void primeNumPrinter(int n){
+
+        for(int i=2;i<=n;i++){
+            boolean isPrime =true;
+           for(int j=2;j<=Math.sqrt(i);j++){
+            if(i%j==0){
+                isPrime=false;
+                break;
+            }
+
+           
+           }
+            if(isPrime){
+                System.out.print(i+" ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        primeNumPrinter(100);
+    }
+}
